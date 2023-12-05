e724a67b5f6c19ca2b6b4a841ac545081a033b2b
SofRozo <111070857+SofRozo@users.noreply.github.com>
2023-12-05 15:07:24 -0500

    llegadas

--- demo/src/main/java/com/example/demo/repositorio/LlegadasRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/LlegadasRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.example.demo.modelo.Clientes;
 import com.example.demo.modelo.Llegadas;
 
 public interface LlegadasRepository extends MongoRepository<Llegadas, String> {
+
+    List<Llegadas> findByCliente(Clientes cliente);
     
 }
