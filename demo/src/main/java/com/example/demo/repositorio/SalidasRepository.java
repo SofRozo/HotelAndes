96cdd09a922497bc80cdc7d28471abfaae281175
SofRozo <111070857+SofRozo@users.noreply.github.com>
2023-12-05 15:37:50 -0500

    salidas

--- demo/src/main/java/com/example/demo/repositorio/SalidasRepository.java
+++ demo/src/main/java/com/example/demo/repositorio/SalidasRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repositorio;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.modelo.Clientes;
+import com.example.demo.modelo.Llegadas;
 import com.example.demo.modelo.Salidas;
 
 public interface SalidasRepository  extends MongoRepository<Salidas, String>{
+
+        List<Salidas> findByCliente(Clientes cliente);
     
 }
